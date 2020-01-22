fc123b8c5d827adaafe97fc6e6e72c5ea2db104d
Mark Strefford <markstrefford@gmail.com>
2020-01-22 15:14:41 +0000

    Adding cubemap images

--- AR-Portal 3/AR-Portal/ViewController.swift
+++ AR-Portal 3/AR-Portal/ViewController.swift
@@ -37,17 +37,32 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     func addPortal(hitTestResult: ARHitTestResult) {
         let portalScene = SCNScene(named: "Portal.scnassets/Portal.scn")
-        let portalNode = portalScene?.rootNode.childNode(withName: "Portal", recursively: false)
+        let portalNode = portalScene!.rootNode.childNode(withName: "Portal", recursively: false)!
         let transform = hitTestResult.worldTransform
         let planeXposition = transform.columns.3.x
         let planeYposition = transform.columns.3.y
         let planeZposition = transform.columns.3.z
-        portalNode?.position = SCNVector3(planeXposition, planeYposition, planeZposition)
-        self.sceneView.scene.rootNode.addChildNode(portalNode!)
-        
-        
+        portalNode.position = SCNVector3(planeXposition, planeYposition, planeZposition)
+        self.sceneView.scene.rootNode.addChildNode(portalNode)
+        self.addPlane(nodeName: "roof", portalNode: portalNode, imageName: "top")
+        self.addPlane(nodeName: "floor", portalNode: portalNode, imageName: "bottom")
+        self.addWalls(nodeName: "sideWallA", portalNode: portalNode, imageName: "sideA")
+        self.addWalls(nodeName: "sideWallB", portalNode: portalNode, imageName: "sideB")
+        self.addWalls(nodeName: "sideDoorA", portalNode: portalNode, imageName: "sideDoorA")
+        self.addWalls(nodeName: "sideDoorB", portalNode: portalNode, imageName: "sideDoorB")
+        self.addWalls(nodeName: "backWall", portalNode: portalNode, imageName: "back")
     }
-
+    
+    func addPlane(nodeName: String, portalNode: SCNNode, imageName: String) {
+        let child = portalNode.childNode(withName: nodeName, recursively: true)
+        child?.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "Portal.scnassets/\(imageName).png")
+    }
+    
+    func addWalls(nodeName: String, portalNode: SCNNode, imageName: String) {
+        let child = portalNode.childNode(withName: nodeName, recursively: true)
+        child?.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "Portal.scnassets/\(imageName).png")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
