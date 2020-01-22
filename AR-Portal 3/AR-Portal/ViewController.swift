5746bab7cc2a29a00b4b387b01f1570bf55e2317
Mark Strefford <markstrefford@gmail.com>
2020-01-22 13:20:15 +0000

    First commit

---
+++ AR-Portal 3/AR-Portal/ViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  AR-Portal
+//
+//  Created by Rayan Slim on 2017-08-31.
+//  Copyright © 2017 Rayan Slim. All rights reserved.
+//
+
+import UIKit
+import ARKit
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet weak var planeDetected: UILabel!
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.configuration.planeDetection = .horizontal
+        self.sceneView.session.run(configuration)
+        self.sceneView.delegate = self
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        guard let sceneView = sender.view as? ARSCNView else {return}
+        let touchLocation = sender.location(in: sceneView)
+        let hitTestResult = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+        if !hitTestResult.isEmpty {
+            self.addPortal(hitTestResult: hitTestResult.first!)
+        } else {
+            //
+        }
+    }
+    
+    func addPortal(hitTestResult: ARHitTestResult) {
+        let portalScene = SCNScene(named: "Portal.scnassets/Portal.scn")
+        let portalNode = portalScene?.rootNode.childNode(withName: "Portal", recursively: false)
+        let transform = hitTestResult.worldTransform
+        let planeXposition = transform.columns.3.x
+        let planeYposition = transform.columns.3.y
+        let planeZposition = transform.columns.3.z
+        portalNode?.position = SCNVector3(planeXposition, planeYposition, planeZposition)
+        self.sceneView.scene.rootNode.addChildNode(portalNode!)
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard anchor is ARPlaneAnchor else {return}
+        DispatchQueue.main.async {
+            self.planeDetected.isHidden = false
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            self.planeDetected.isHidden = true
+        }
+    }
+    
+}
+
